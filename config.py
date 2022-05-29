aaa4e25e5e738e7c659ced89ddbe02bc7f9d1157
Proton03 <105384297+Proton03@users.noreply.github.com>
2022-05-29 18:36:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from pyrogram.types import ReplyKeyboardMarkup,InlineKeyboardMarkup, InlineKeybo
 
 load_dotenv("config.env")
 
-BOT_OWNER = 1984415770
+BOT_OWNER = 5113561442
 BOT_TOKEN = environ.get("BOT_TOKEN", None)
 API_ID = int(environ.get("API_ID", 6))
 API_HASH = environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
